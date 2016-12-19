dfb6bc3f630bc2b85f66d8fe3481edf8b3230cee
Hanna <hanna_autio@hotmail.com>
2016-12-19 21:07:34 +0100

    Added some further checks in the linesearch, wrote an initial test script for testing.

--- linesearch_armijo.m
+++ linesearch_armijo.m
@@ -1,4 +1,7 @@
-function [lambda, n_iter] = linesearch_armijo(func, x, dir)
+function [lambda, n_iter] = linesearch_armijo(func, x, dir, tol)
+    if nargin < 4
+        tol = 1e-6;
+    end
     epsilon = 1e-4;
     alpha = 2;
     n_iter = 1;
@@ -12,13 +15,21 @@ function [lambda, n_iter] = linesearch_armijo(func, x, dir)
         elseif (func(x + dir*alpha*lambda) < f_0 + epsilon*alpha*lambda*df_0)
             lambda = 2*lambda;
         elseif (isnan(func(x + dir*lambda)) || isinf(func(x + dir*lambda)))
-            disp('WARNING, line search: Lambda set to 0, as a unit step in the chosen direction gives inf or NaN values.')
-            lambda = 0;
-            return;
+            lambda = tol;
+            if (isnan(func(x + dir*lambda)) || isinf(func(x+dir*lambda)))
+                disp('WARNING, line search: Lambda set to 0, as a small step in the chosen direction gives inf or NaN values.')
+                lambda = 0;
+                return;
+            end
         else
             break
         end
         n_iter = n_iter + 1;
+        if (n_iter > 50)
+            disp('WARNING, line search: Lambda set to 0, as max number of iterations were reached.')
+            lambda = 0;
+            return;
+        end
     end
     if isnan(func(x+lambda*dir)) || func(x+lambda*dir)>func(x)
         error('Bad job of the line search!')
